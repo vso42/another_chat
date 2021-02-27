5557d5fa7ceeae68ea91806d238ef6c065aef6cb
vso42 <37542449+vso42@users.noreply.github.com>
2021-02-27 10:03:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const path =require('path');
+
+const http= require('http');
+
+const express=require('express');
+const socketio=require('socket.io');
+
+const app = express();
+const server=http.createServer(app);
+const io = socketio(server); 
+
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',socket=>{
+    console.log('new con..');
+    socket.emit('message', 'Welcome to ChatOn');  
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen (PORT, ()=> console.log('server running on port ${PORT}'));
